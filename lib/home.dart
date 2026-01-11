6d108eb92ffdab2e63b764a5b0b5663ff0e41180
Dnts77 <daniel.souzadantas07@gmail.com>
2026-01-11 15:33:31 -0300

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  static String _preco = "0";
+
+  void _recuperarPreco() async {
+    String url = "https://blockchain.info/ticker";
+
+    http.Response response = await http.get(Uri.parse(url));
+
+    Map<String, dynamic> retorno = json.decode(response.body);
+
+    setState(() {
+      _preco = retorno["BRL"] ["buy"].toString();
+    });
+
+    
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("imgs/bitcoin.png"),
+              Padding(
+                padding: EdgeInsets.only(top: 30, bottom: 30),
+                child: Text(
+                  "R\$ $_preco",
+                  style: TextStyle(
+                    fontSize: 35
+                  ),
+                  ),
+                ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.orange,
+                  padding: EdgeInsets.fromLTRB(30, 15, 30, 15),
+                ),
+                onPressed: _recuperarPreco, 
+                child: Text(
+                  "Atualizar",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white
+                  ),
+                  )
+                )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
